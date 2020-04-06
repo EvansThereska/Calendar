006db2497c6e5752bf8504afb601e6c7639d41bc
Evans Thereska <Evans.Thereska@prax.com>
2020-04-06 14:26:28 +0100

    Drop down is now ready. Clicking on a button an having textbox display date is next

--- PraxGroup.Sorax.CapacityCalendar.Main/CapacityDateTimePicker.cs
+++ PraxGroup.Sorax.CapacityCalendar.Main/CapacityDateTimePicker.cs
@@ -1,18 +1,61 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace PraxGroup.Sorax.CapacityCalendar.Main
 {
-    public class CapacityDateTimePicker
+    public class CapacityDateTimePicker : UserControl
     {
-        private ButtonTextBox _textBox;
+        private ButtonTextBox _btnTxtBox;
         private CapacityCalendar _calendar;
 
-        public Point Location
+        private bool _isVisible;
+
+        public CapacityDateTimePicker()
         {
-            get => _textBox.Location;
-            set => _textBox.Location = value;
+            InitializeComponents();
+            _btnTxtBox.ButtonClick += OnButtonClick;
+        }
+
+        private void InitializeComponents()
+        {
+            _btnTxtBox = new ButtonTextBox();
+            _calendar = new CapacityCalendar();
+            SuspendLayout();
+
+            // this._btnTxtBox.Location = new System.Drawing.Point(505, 27);
+            // this._btnTxtBox.Name = "_btnTxtBox";
+            // this._btnTxtBox.ReadOnly = true;
+
+            _calendar.Visible = false;
+            _btnTxtBox.Size = new System.Drawing.Size(210, 20);
+            // this._btnTxtBox.TabIndex = 0;
+            // this._btnTxtBox.Visible = true;
+            Controls.Add(_btnTxtBox);
+            Controls.Add(_calendar);
+            ResumeLayout(false);
+        }
+
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            if (_isVisible)
+            {
+                _calendar.Visible = false;
+                _isVisible = false;
+                return;
+            }
+            
+            var xPoint = _btnTxtBox.Location.X;
+            var yPoint = _btnTxtBox.Location.Y + _btnTxtBox.Height;
+            _calendar.Location = new Point(xPoint, yPoint);
+            _calendar.BackColor = Color.White;
+            _calendar.Size = new Size(_btnTxtBox.Width + 40, 200);
+            _calendar.HighlightCurrentDay = true;
+            _calendar.Visible = true;
+            _isVisible = true;
         }
 
 
+        
     }
 }
